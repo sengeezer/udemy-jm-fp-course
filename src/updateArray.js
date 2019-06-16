d4af879e425df0e8e48a5ae62c85164ee419d08f
Fabio Albertin <albertin@gmail.com>
2019-06-16 03:30:10 +0100

    Completed chapter 14

--- src/updateArray.js
+++ src/updateArray.js
@@ -1,1 +1,25 @@
 // Updating arrays
+const meals = [
+  { id: 1, description: 'Breakfast', calories: 420 },
+  { id: 2, description: 'Lunch', calories: 520 },
+  { id: 3, description: 'Snack', calories: 100 },
+];
+
+function updateDescription(meal) {
+  if (meal.id === 2) {
+    return {
+      ...meal,
+      description: 'Early lunch',
+    };
+  }
+
+  return meal;
+}
+
+const updatedMealDescription = meals.map(updateDescription);
+
+console.log(updatedMealDescription);
+
+const filteredMeals = meals.filter(meal => meal.id !== 1);
+
+console.log(filteredMeals);
