653ff0415c5be238749cd18816f0b5fd7a945eb5
Fabio Albertin <albertin@gmail.com>
2019-06-18 23:51:02 +0100

    Finished chapter 15

--- src/summarizeArray.js
+++ src/summarizeArray.js
@@ -17,3 +17,37 @@ const grades = [60, 55, 80, 90, 99, 92, 75, 72];
 const avgGrade = grades.reduce(sum) / grades.length;
 
 console.log(avgGrade);
+
+function groupByGrade(acc, grade) {
+  let {
+    a = 0,
+    b = 0,
+    c = 0,
+    d = 0,
+    f = 0,
+  } = acc;
+
+  if (grade >= 90) {
+    return { ...acc, a: a++ };
+  }
+
+  if (grade >= 80) {
+    return { ...acc, b: b++ };
+  }
+
+  if (grade >= 70) {
+    return { ...acc, c: c++ };
+  }
+
+  if (grade >= 60) {
+    return { ...acc, d: d++ };
+  }
+
+  if (grade < 60) {
+    return { ...acc, f: f++ };
+  }
+}
+
+const letterGradeCount = grades.reduce(groupByGrade, {});
+
+console.log(letterGradeCount);
